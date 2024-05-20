17ec2fa6031bf18e50d5598580bcb0ce1830b96e
TranHung-98 <hung20101998@gmail.com>
2024-05-20 14:39:04 +0700

    create-edit: target-user

--- src/app/modules/projects/components/projects-edit-or-create/target-user/target-user.component.spec.ts
+++ src/app/modules/projects/components/projects-edit-or-create/target-user/target-user.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TargetUserComponent } from './target-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('TargetUserComponent', () => {
   let component: TargetUserComponent;
@@ -8,7 +9,8 @@ describe('TargetUserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TargetUserComponent]
+      declarations: [TargetUserComponent],
+      imports: [ReactiveFormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(TargetUserComponent);
--- src/app/modules/projects/components/projects-edit-or-create/target-user/target-user.component.ts
+++ src/app/modules/projects/components/projects-edit-or-create/target-user/target-user.component.ts
@@ -1,13 +1,13 @@
 import { Subscription } from 'rxjs';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AddEditApiService } from '../service/add-edit-api.service';
 import { EStatusSelect } from 'src/app/enums/status-filter.enum';
+import { AddEditApiService } from '../service/add-edit-api.service';
 import { AddEditFormService } from '../service/add-edit-form.service';
-import { IUserNotPaddingResponse } from 'src/app/interfaces/add-edit-project.interface';
-import { IProjectForm, ITargetUserFormGroup } from 'src/app/interfaces/add-edit-project-form.interface';
 import { AddEditControllService } from '../service/add-edit-controll.service';
+import { IProjectForm, ITargetUserFormGroup } from 'src/app/interfaces/add-edit-project-form.interface';
+import { IProjectTargetUsersResponse, IUserNotPaddingResponse } from 'src/app/interfaces/add-edit-project.interface';
 
 
 @Component({
@@ -17,65 +17,94 @@ import { AddEditControllService } from '../service/add-edit-controll.service';
 })
 export class TargetUserComponent implements OnInit, OnDestroy {
   roleName: string = '';
+  status: boolean = false;
   searchUser: string = '';
-  userList!: IUserNotPaddingResponse[];
   getUserSubscription!: Subscription;
-  userListShow!: IUserNotPaddingResponse[];
+  checkIdSubscription!: Subscription;
+  userList!: IUserNotPaddingResponse[];
+  projectForm!: FormGroup<IProjectForm>;
+  userRemoved!: IUserNotPaddingResponse[];
   selectedFilter: string = EStatusSelect.ALL;
+  targetUser: IProjectTargetUsersResponse[] = [];
+  targetUserOrther: IProjectTargetUsersResponse[] = [];
   activeUserList: IUserNotPaddingResponse[] = [];
   selectFilters = [EStatusSelect.ALL, EStatusSelect.ACTIVE, EStatusSelect.INACTIVE,];
-  projectForm!: FormGroup<IProjectForm>;
+
+
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private projectApiService: AddEditApiService,
     private projectFormService: AddEditFormService,
-    private addEditControllService: AddEditControllService,
-
-  ) { }
+    public addEditControllService: AddEditControllService,
+  ) {
+    if (!this.projectFormService.checkFormFieldTasksData()) {
+      this.backRoute();
+    }
+  }
 
   ngOnDestroy(): void {
     this.getUserSubscription.unsubscribe();
+    if (this.checkIdSubscription) {
+      this.checkIdSubscription.unsubscribe();
+    }
   }
 
   ngOnInit(): void {
     this.getUserSubscription = this.projectApiService.getUsers().subscribe((res) => {
       this.userList = res?.result;
-      this.userListShow = this.userList;
+      this.userRemoved = this.userList;
+      if (this.targetUserArrayForm.length > 0) {
+        this.targetUserArrayForm.controls.forEach(control => {
+          this.userList = this.userList.filter(user => user.id != control.value.userId);
+        });
+      }
     });
-    this.projectForm = this.projectFormService.projectForm;
   }
 
   get targetUserArrayForm() {
     return this.projectFormService.getTargetUserArrayForm();
   }
 
-  createTargetUserForm(user: ITargetUserFormGroup) {
-    this.projectForm.controls.targetUserArrayForm?.push(this.fb.group<ITargetUserFormGroup>({
+  createTargetUserForm(user: IProjectTargetUsersResponse) {
+    return this.fb.group<ITargetUserFormGroup>({
       userId: this.fb.control<number | null>(user.userId),
       roleName: this.fb.control<string | null>(user.roleName),
-    }));
-    // eslint-disable-next-line no-console
-    console.log(this.projectForm.controls.targetUserArrayForm);
+    });
   }
 
   removeUserItemForm(userId: number) {
-    if (Array.isArray(this.activeUserList)) {
-      const index = this.activeUserList.findIndex(user => user.id === userId);
-      if (index !== -1) {
-        this.activeUserList.splice(index, 1);
-      }
+    const index = this.targetUserArrayForm.controls.findIndex(control => control.value.userId === userId);
+    const user = this.userRemoved.find(user => user.id === userId);
+    if (index !== -1 && user) {
+      this.targetUserArrayForm.removeAt(index);
+      this.userList.push(user);
+      this.userRemoved = this.userRemoved.filter(user => user.id !== userId);
     }
   }
 
-  returnFormGroup() {
-    if (this.projectForm.controls.targetUserArrayForm?.controls && this.projectForm.controls.targetUserArrayForm?.controls.length > 0) {
-      return this.projectForm.controls.targetUserArrayForm?.controls;
+  getNameById(id: number) {
+    if (this.userRemoved) {
+      const data = this.userRemoved.find(user => user.id === id);
+      if (data) {
+        this.status = data.isActive;
+        return data.name;
+      }
     }
-    else {
-      return this.projectForm.controls.targetUserArrayForm?.controls;
+    return 'Loading...';
+  }
+
+
+  getEmailById(id: number) {
+    if (this.userRemoved) {
+      const data = this.userRemoved.find(user => user.id === id);
+      if (data) {
+        this.status = data.isActive;
+        return data.emailAddress;
+      }
     }
+    return 'Loading...';
   }
 
   nextRoute() {
@@ -85,30 +114,27 @@ export class TargetUserComponent implements OnInit, OnDestroy {
   }
 
   handleAddUser(userId: number) {
-    const userItem = this.addEditControllService.handleAddUser(userId, this.userList);
-    if (userItem) {
-      const targetFormGroupPayload: ITargetUserFormGroup = {
-        userId: new FormControl(userItem.id),
-        roleName: new FormControl(''),
-      };
-      this.createTargetUserForm(targetFormGroupPayload);
+    const user = this.addEditControllService.handleFindUserById(userId, this.userList);
 
-      this.activeUserList.push(userItem);
+    if (user) {
+      const targetUserForm = this.fb.group<ITargetUserFormGroup>({
+        userId: this.fb.control<number>(user.id),
+        roleName: this.fb.control<string>(''),
+      });
+      this.targetUserArrayForm.push(targetUserForm);
+      this.userRemoved.push(user);
       this.userList = this.userList.filter(user => user.id != userId);
-      this.userListShow = this.userList;
-
     }
   }
 
   backRoute() {
-    this.router.navigate(['../team',], { relativeTo: this.route });
+    this.router.navigate(['../tasks',], { relativeTo: this.route });
   }
 
   handleSearch(): void {
     const searchTerm = this.searchUser.toLowerCase();
-    this.userListShow = this.userList.filter(user =>
+    this.userList = this.userList.filter(user =>
       user.name.toLowerCase().includes(searchTerm) || user.emailAddress.toLowerCase().includes(searchTerm));
-
   }
 
   handleFilter() {
@@ -116,9 +142,9 @@ export class TargetUserComponent implements OnInit, OnDestroy {
       this.selectedFilter === EStatusSelect.INACTIVE ? false : true;
 
     if (typeof filter === 'boolean') {
-      this.userListShow = this.userList.filter(user => user.isActive === filter);
+      this.userList = this.userList.filter(user => user.isActive === filter);
     } else {
-      this.userListShow = this.userList;
+      this.userList;
     }
   }
 
